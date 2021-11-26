c20ed845911a1939b227148fa2f656fc2e7e8559
François Lefebvre <francois.lfe@gmail.com>
2021-11-26 23:52:59 +0100

    wip

--- src/tests/prisma.test.ts
+++ src/tests/prisma.test.ts
@@ -1,30 +1,14 @@
 import prisma from '../client';
 import { prismaMock } from '../setupTests';
+import user from './fakes/user';
 
 describe('Prisma mocks', () => {
   it('should be able to mock prisma resolved values', async () => {
-    const user = {
-      userId: '1',
-      email: 'test@test.fr',
-      password: 'password',
-      firstName: 'John',
-      lastName: 'Doe',
-      isDisabled: false,
-      createdAt: new Date(),
-      updatedAt: new Date(),
-    };
     await prismaMock.user.create.mockResolvedValue(user);
     await expect(
       prisma.user.create({
         data: user,
       })
-    ).resolves.toMatchObject({
-      userId: '1',
-      email: 'test@test.fr',
-      password: 'password',
-      firstName: 'John',
-      lastName: 'Doe',
-      isDisabled: false,
-    });
+    ).resolves.toMatchObject(user);
   });
 });
