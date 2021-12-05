6f8faaf4fd6649fb851b24a5d329b8ca98c4eb55
François Lefebvre <francois.lfe@gmail.com>
2021-12-05 23:26:24 +0100

    wip

--- src/app.ts
+++ src/app.ts
@@ -1,12 +1,22 @@
 import express, { Application } from 'express';
+import { body, validationResult } from 'express-validator';
 
 const app: Application = express();
 
 // Parse incoming requests with JSON payloads
 app.use(express.json() as express.RequestHandler);
 
-app.post('/login', (req, res) => {
-  res.send('Coucou');
-});
+app.post(
+  '/login',
+  body('email').isEmail(),
+  body('password').isString(),
+  (req, res) => {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      return res.status(400).json({ errors: errors.array() });
+    }
+    res.send('Coucou');
+  }
+);
 
 export default app;
--- src/tests/auth.test.ts
+++ src/tests/auth.test.ts
@@ -4,7 +4,11 @@ import app from '../app';
 
 describe('Authentication', () => {
   it('should return a 401 http status code', async () => {
-    const result = await request(app).get('/');
+    const result = await request(app).post('/login').send({
+      email: 'coucou@test.fr',
+      password: '',
+    });
+    console.log(result.body);
     expect(result.statusCode).toBe(401);
   });
 });
