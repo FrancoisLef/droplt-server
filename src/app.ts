e788174df2f1c5421b521b2b31657b1d909616da
François Lefebvre <francois.lfe@gmail.com>
2021-12-07 00:56:58 +0100

    wip

--- src/app.ts
+++ src/app.ts
@@ -1,20 +1,26 @@
-import express, { Application } from 'express';
-import { body, validationResult } from 'express-validator';
+import express, {
+  Application,
+  json,
+  Request,
+  RequestHandler,
+  Response,
+} from 'express';
+import { check } from 'express-validator';
+
+import { validate } from './modules/express';
 
 const app: Application = express();
 
 // Parse incoming requests with JSON payloads
-app.use(express.json() as express.RequestHandler);
+app.use(json() as RequestHandler);
 
 app.post(
   '/login',
-  body('email').isEmail(),
-  body('password').isString(),
-  (req, res) => {
-    const errors = validationResult(req);
-    if (!errors.isEmpty()) {
-      return res.status(400).json({ errors: errors.array() });
-    }
+  validate([
+    check('email').isEmail().normalizeEmail(),
+    check('password').notEmpty().isString(),
+  ]),
+  (req: Request, res: Response) => {
     res.send('Coucou');
   }
 );
