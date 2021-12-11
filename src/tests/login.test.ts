515ce18e2d86537e5117d9a2e299d0d86dffba90
François Lefebvre <francois.lfe@gmail.com>
2021-12-11 23:02:50 +0100

    wip

--- src/tests/login.test.ts
+++ src/tests/login.test.ts
@@ -5,39 +5,44 @@ import app from '../app';
 import { prismaMock } from '../setupTests';
 import user from './fakes/user';
 
+const agent = request.agent(app);
 const { email, password } = user;
 
 describe('Auth - login', () => {
   it('should be served on endpoint /login', async () => {
-    const post = await request(app).post('/login').send();
+    const post = await agent.post('/login').send();
     expect(post.statusCode).not.toBe(404);
   });
 
   it('should return a 400 on invalid credentials', async () => {
-    const result = await request(app).post('/login').send({});
-    expect(result.statusCode).toBe(400);
-    expect(result.body.errors).toEqual(
-      expect.arrayContaining([
-        expect.objectContaining({
-          param: 'email',
-          location: 'body',
-        }),
-        expect.objectContaining({
-          param: 'password',
-          location: 'body',
-        }),
-      ])
-    );
+    return agent
+      .post('/login')
+      .expect(400)
+      .expect(({ body }) => {
+        expect(body.errors).toEqual(
+          expect.arrayContaining([
+            expect.objectContaining({
+              param: 'email',
+              location: 'body',
+            }),
+            expect.objectContaining({
+              param: 'password',
+              location: 'body',
+            }),
+          ])
+        );
+      })
+      .send({});
   });
 
   it('should return a 404 on unknown email', async () => {
-    const result = await request(app).post('/login').send({ email, password });
+    const result = await agent.post('/login').send({ email, password });
     expect(result.statusCode).toBe(404);
   });
 
   it('should return a 404 on wrong password', async () => {
     prismaMock.user.findUnique.mockResolvedValue(user);
-    const result = await request(app).post('/login').send({ email, password });
+    const result = await agent.post('/login').send({ email, password });
     expect(result.statusCode).toBe(404);
   });
 
@@ -46,9 +51,10 @@ describe('Auth - login', () => {
     const bcryptCompare = jest.fn().mockResolvedValue(true);
     (bcrypt.compare as jest.Mock) = bcryptCompare;
 
-    const result = await request(app).post('/login').send({ email, password });
+    const result = await agent.post('/login').send({ email, password });
     console.log(result.body);
     console.log(result.headers);
+    expect(result.headers);
     expect(result.statusCode).toBe(200);
   });
 });
