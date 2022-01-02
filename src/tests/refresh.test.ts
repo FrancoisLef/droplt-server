ee4c4d1653a6e3ea0241b680d2fc47c35bf4819c
François Lefebvre <francois.lfe@gmail.com>
2022-01-02 15:20:54 +0100

    wip

--- src/tests/refresh.test.ts
+++ src/tests/refresh.test.ts
@@ -1,17 +1,8 @@
-import jwt from 'jsonwebtoken';
 import { SuperAgentTest } from 'supertest';
 
-import {
-  getCookie,
-  getUser,
-  mockBcryptCompare,
-  mockFindUniqueUser,
-  useAgent,
-} from './helpers';
+import { useAgent } from './helpers';
 
 let agent: SuperAgentTest;
-const user = getUser();
-const { email, password } = user;
 const URL = '/api/refresh';
 
 describe('Auth - refresh', () => {
@@ -23,58 +14,7 @@ describe('Auth - refresh', () => {
     return agent.get(URL).expect(401);
   });
 
-  it('should return a 401 on invalid refresh_token cookie', async () => {
+  it('should return a 401 on invalid refresh_token', async () => {
     return agent.get(URL).set('Cookie', 'refresh_token=test').expect(401);
   });
-
-  // it('should return a 404 on unknown email', async () => {
-  //   return agent.post('/login').expect(404).send({ email, password });
-  // });
-
-  // it('should return a 404 on wrong password', async () => {
-  //   mockFindUniqueUser(user);
-  //   return agent.post('/login').expect(404).send({ email, password });
-  // });
-
-  // it('should return a JWT on successful authentication', async () => {
-  //   mockFindUniqueUser(user);
-  //   mockBcryptCompare(true);
-
-  //   return agent
-  //     .post('/login')
-  //     .expect(200)
-  //     .expect(({ body }) => {
-  //       expect(jwt.decode(body.token)).toEqual(
-  //         expect.objectContaining({
-  //           userId: user.userId,
-  //           firstName: user.firstName,
-  //           lastName: user.lastName,
-  //         })
-  //       );
-  //     })
-  //     .send({ email, password });
-  // });
-
-  // it('should return a refresh_token as a cookie', async () => {
-  //   mockFindUniqueUser(user);
-  //   mockBcryptCompare(true);
-
-  //   return agent
-  //     .post('/login')
-  //     .expect(200)
-  //     .expect(({ headers }) => {
-  //       const refreshToken = getCookie(
-  //         'refresh_token',
-  //         headers['set-cookie'][0]
-  //       );
-  //       expect(jwt.decode(refreshToken)).toEqual(
-  //         expect.objectContaining({
-  //           userId: user.userId,
-  //           firstName: user.firstName,
-  //           lastName: user.lastName,
-  //         })
-  //       );
-  //     })
-  //     .send({ email, password });
-  // });
 });
