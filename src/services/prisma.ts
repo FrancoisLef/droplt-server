ce4a388ffd1a88943c7e4965436ffe4e925894b8
François Lefebvre <francois.lfe@gmail.com>
2022-02-07 23:24:17 +0100

    feat(wip): wip

--- src/services/prisma.ts
+++ src/services/prisma.ts
@@ -1,5 +1,19 @@
 import { PrismaClient } from '@prisma/client';
 
-const prisma: PrismaClient = new PrismaClient();
+const {
+  POSTGRES_DB,
+  POSTGRES_USER,
+  POSTGRES_PASSWORD,
+  POSTGRES_HOST = 'localhost',
+  POSTGRES_PORT = 5432,
+} = process.env;
+
+const prisma: PrismaClient = new PrismaClient({
+  datasources: {
+    db: {
+      url: `postgresql://${POSTGRES_USER}:${POSTGRES_PASSWORD}@${POSTGRES_HOST}:${POSTGRES_PORT}/${POSTGRES_DB}`,
+    },
+  },
+});
 
 export default prisma;
