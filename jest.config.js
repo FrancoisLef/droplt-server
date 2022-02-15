ed3326c2313137dfd9b468f4b2a0aedeb8de684b
François Lefebvre <francois.lfe@gmail.com>
2022-02-15 20:55:10 +0100

    feat(wip): wip

--- jest.config.js
+++
@@ -1,8 +0,0 @@
-/** @type {import('ts-jest/dist/types').InitialOptionsTsJest} */
-module.exports = {
-  preset: 'ts-jest',
-  clearMocks: true,
-  testEnvironment: 'node',
-  setupFiles: ['<rootDir>/src/bootstrap.ts'],
-  setupFilesAfterEnv: ['<rootDir>/src/setupTests.ts'],
-};
